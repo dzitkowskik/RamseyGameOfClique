be5ea9dfd77827dddea20c4bbd6cfaa0a4be3753
Tomasz Janiszewski <tomasz.janiszewski@allegrogroup.com>
2015-11-16 15:59:27 +0100

    Implement ramsey.js

--- app/js/ramsey.js
+++ app/js/ramsey.js
@@ -1,6 +1,43 @@
 function findClique(graph, minimalCliqueSize) {
-// TODO[janisz]: IMPLEMENT ME.
-      console.log("Given graph: ", graph);
-      console.log("Minimal Clique Size: ", minimalCliqueSize);
-      return true;
+      var cliques = allCliques(graph);
+      return typeof(cliques.find(function(clique) {
+        return clique.length >= minimalCliqueSize;
+      })) != "undefined";
+}
+
+
+// http://stackoverflow.com/a/28559315/1387612
+function allCliques(g){
+    var cliques=[];
+    var p=[];
+    for (var node in g) {
+      if (g.hasOwnProperty(node)) {
+        p.push(node);
+      }
+    }
+
+    var r =[];
+    var x=[];
+
+    bronKerbosch(g, r, p, x, cliques);
+    return cliques;
+}
+
+function bronKerbosch(g, r, p, x, cliques) {
+    if (p.length===0 && x.length===0){
+        cliques.push(r);
+    }
+
+    p.forEach(function(v){
+        var tempR= r.splice(0);
+        tempR.push(v);
+        bronKerbosch(g, tempR, p.filter(function(temp){
+            return g[v].indexOf(temp)!=-1;
+        }), x.filter(function(temp){
+            return g[v].indexOf(temp)!=-1;
+        }), cliques);
+
+        p.splice(p.indexOf(v),1);
+        x.push(v);
+    });
 }
